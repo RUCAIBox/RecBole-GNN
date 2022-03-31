0eaf1c77a48e48cad39b8a2b7fa5be34eac8a931
Sherry-XLL <xulanling_sherry@163.com>
2022-03-31 21:54:26 +0000

    FIX: rename graph to gnn

--- recbole_gnn/model/social_recommender/__init__.py
+++ recbole_gnn/model/social_recommender/__init__.py
@@ -1,3 +1,4 @@
 from recbole_gnn.model.social_recommender.diffnet import DiffNet
 from recbole_gnn.model.social_recommender.graphrec import GraphRec
+from recbole_gnn.model.social_recommender.mhcn import MHCN
 from recbole_gnn.model.social_recommender.sept import SEPT
