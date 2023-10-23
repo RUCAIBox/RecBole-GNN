fc37afca8266bf84cb17763861775601747d6b7c
xzjin <downeykking@gmail.com>
2023-10-23 21:40:24 +0800

    FEA: add sparse tensor support for ngcf_conv and lightgcn_conv

--- recbole_gnn/model/abstract_recommender.py
+++ recbole_gnn/model/abstract_recommender.py
@@ -12,8 +12,12 @@ class GeneralGraphRecommender(GeneralRecommender):
 
     def __init__(self, config, dataset):
         super(GeneralGraphRecommender, self).__init__(config, dataset)
-        self.edge_index, self.edge_weight = dataset.get_norm_adj_mat()
-        self.edge_index, self.edge_weight = self.edge_index.to(self.device), self.edge_weight.to(self.device)
+        self.edge_index, self.edge_weight = dataset.get_norm_adj_mat(enable_sparse=config["enable_sparse"])
+        self.use_sparse = config["enable_sparse"] and dataset.is_sparse
+        if self.use_sparse:
+            self.edge_index, self.edge_weight = self.edge_index.to(self.device), None
+        else:
+            self.edge_index, self.edge_weight = self.edge_index.to(self.device), self.edge_weight.to(self.device)
 
 
 class SocialRecommender(GeneralRecommender):
